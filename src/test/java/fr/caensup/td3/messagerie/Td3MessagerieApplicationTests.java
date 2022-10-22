3843d5734e7a1ddf3bab47b19d7eb2d366f8642a
jcheron <myaddressmail@gmail.com>
2022-10-22 12:19:01 +0200

    Update Td3MessagerieApplicationTests.java

--- src/test/java/fr/caensup/td3/messagerie/Td3MessagerieApplicationTests.java
+++ src/test/java/fr/caensup/td3/messagerie/Td3MessagerieApplicationTests.java
@@ -2,13 +2,25 @@ package fr.caensup.td3.messagerie;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class Td3MessagerieApplicationTests {
+
+  @Autowired
+  private WebApplicationContext context;
+
+  @Value("server.contextPath")
+  private String contextPath;
 
   @Autowired
   private MockMvc mvc;
@@ -16,9 +28,14 @@ class Td3MessagerieApplicationTests {
   @Test
   void contextLoads() {}
 
+
+  @BeforeAll
+  public void setUp() {
+    this.mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+  }
+
   @Test
   void indexTest() throws Exception {
-    String path = mvc.getDispatcherServlet().getServletContext().getContextPath();
-    this.mvc.perform(get(path)).andExpect(content().string("Ajouter une organisation"));
+    this.mvc.perform(get(contextPath)).andExpect(content().string("Ajouter une organisation"));
   }
 }
