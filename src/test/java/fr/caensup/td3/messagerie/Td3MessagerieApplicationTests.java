d5ea2811118f20eba387279a91ad0882f75ddfe0
jcheron <myaddressmail@gmail.com>
2022-10-22 12:42:38 +0200

    Update Td3MessagerieApplicationTests.java

--- src/test/java/fr/caensup/td3/messagerie/Td3MessagerieApplicationTests.java
+++ src/test/java/fr/caensup/td3/messagerie/Td3MessagerieApplicationTests.java
@@ -4,7 +4,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
@@ -13,16 +12,15 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureMockMvc
 class Td3MessagerieApplicationTests {
 
-  @Value("server.contextPath")
-  private String contextPath;
-
-  @Autowired private MockMvc mvc;
+  @Autowired
+  private MockMvc mvc;
 
   @Test
   void contextLoads() {}
 
   @Test
   void indexTest() throws Exception {
-    this.mvc.perform(get(contextPath)).andExpect(content().string("Ajouter une organisation"));
+    String path = mvc.getDispatcherServlet().getServletContext().getContextPath();
+    this.mvc.perform(get(path)).andExpect(content().string("Ajouter une organisation"));
   }
 }
