b960d46ade589e7ab26f5795419fbd80ce2e7a7d
jcheron <myaddressmail@gmail.com>
2022-11-18 01:51:32 +0100

    remove security in tests

--- src/test/java/fr/caensup/td3/messagerie/Td3MessagerieApplicationTests.java
+++ src/test/java/fr/caensup/td3/messagerie/Td3MessagerieApplicationTests.java
@@ -7,15 +7,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import fr.caensup.td3.messagerie.config.WebSecurityConfig;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ContextConfiguration(classes = {Td3MessagerieApplication.class, WebSecurityConfig.class})
-@EnableWebSecurity
+@TestPropertySource(properties = {"security.test=false"})
 class Td3MessagerieApplicationTests {
   @Autowired
   private MockMvc mvc;
@@ -28,6 +25,6 @@ class Td3MessagerieApplicationTests {
   @Test
   void indexTest() throws Exception {
     this.mvc.perform(get(contextPath))
-        .andExpect(content().string(containsStringIgnoringCase("Connexion à Messagerie App")));
+        .andExpect(content().string(containsStringIgnoringCase("Ajouter une organisation")));
   }
 }
