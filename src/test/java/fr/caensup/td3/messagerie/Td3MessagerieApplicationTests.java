0e5ac30b8ff1be17bee39d4348201ff8f68e16f7
jcheron <myaddressmail@gmail.com>
2022-11-18 18:07:43 +0100

    Update Td3MessagerieApplicationTests.java

--- src/test/java/fr/caensup/td3/messagerie/Td3MessagerieApplicationTests.java
+++ src/test/java/fr/caensup/td3/messagerie/Td3MessagerieApplicationTests.java
@@ -1,15 +1,24 @@
 package fr.caensup.td3.messagerie;
 
+import static org.hamcrest.CoreMatchers.containsStringIgnoringCase;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@ContextConfiguration
+@WebAppConfiguration
 @ActiveProfiles("test")
 
 class Td3MessagerieApplicationTests {
@@ -18,15 +27,21 @@ class Td3MessagerieApplicationTests {
 
   private String contextPath = "/";
 
-  @Test
-  void contextLoads() {}
+  @Autowired
+  private WebApplicationContext context;
+
+
+  @BeforeAll
+  public void setup() {
+    mvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+  }
 
   @Test
   @WithAnonymousUser
   void indexTest() throws Exception {
-    /*
-     * this.mvc.perform(get(contextPath))
-     * .andExpect(content().string(containsStringIgnoringCase("Connexion à votre compte")));
-     */
+
+    this.mvc.perform(get(contextPath))
+        .andExpect(content().string(containsStringIgnoringCase("Connexion à Messagerie App")));
+
   }
 }
