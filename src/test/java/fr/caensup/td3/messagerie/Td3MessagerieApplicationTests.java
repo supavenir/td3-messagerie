9bcf931e7dc4509c9cb938fadbb45696a030b76b
jcheron <myaddressmail@gmail.com>
2022-11-18 02:20:13 +0100

    add springboot test dep for security

--- src/test/java/fr/caensup/td3/messagerie/Td3MessagerieApplicationTests.java
+++ src/test/java/fr/caensup/td3/messagerie/Td3MessagerieApplicationTests.java
@@ -5,15 +5,12 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
-@AutoConfigureMockMvc(addFilters = false)
-@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
+@AutoConfigureMockMvc
 class Td3MessagerieApplicationTests {
   @Autowired
   private MockMvc mvc;
@@ -26,6 +23,6 @@ class Td3MessagerieApplicationTests {
   @Test
   void indexTest() throws Exception {
     this.mvc.perform(get(contextPath))
-        .andExpect(content().string(containsStringIgnoringCase("Ajouter une organisation")));
+        .andExpect(content().string(containsStringIgnoringCase("Connexion à votre compte")));
   }
 }
