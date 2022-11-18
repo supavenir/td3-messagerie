390670a4452b106380eb29bc539bfc126c0e674e
jcheron <myaddressmail@gmail.com>
2022-11-18 02:27:54 +0100

    Update Td3MessagerieApplicationTests.java

--- src/test/java/fr/caensup/td3/messagerie/Td3MessagerieApplicationTests.java
+++ src/test/java/fr/caensup/td3/messagerie/Td3MessagerieApplicationTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -24,6 +25,7 @@ class Td3MessagerieApplicationTests {
   void contextLoads() {}
 
   @Test
+  @WithAnonymousUser
   void indexTest() throws Exception {
     this.mvc.perform(get(contextPath))
         .andExpect(content().string(containsStringIgnoringCase("Connexion à votre compte")));
