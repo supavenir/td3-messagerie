e9cc8ff2a249ee2bb5099053261812d99d356e16
jcheron <myaddressmail@gmail.com>
2022-11-18 02:25:58 +0100

    Update Td3MessagerieApplicationTests.java

--- src/test/java/fr/caensup/td3/messagerie/Td3MessagerieApplicationTests.java
+++ src/test/java/fr/caensup/td3/messagerie/Td3MessagerieApplicationTests.java
@@ -7,12 +7,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@EnableWebSecurity
+@ActiveProfiles("test")
+
 class Td3MessagerieApplicationTests {
   @Autowired
   private MockMvc mvc;
